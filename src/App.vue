6a05b66171c57d3058c4dd50e2c573cd3bd4514f
Hesnan Ávila <avilahesnan@outlook.com>
2024-04-08 11:22:38 -0300

    feat: add animations and transitions

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinando transição/animação de elementos/components no Vue.</p>
+      </div>
+    </div>
+
+    <div class="container">
+      <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
+
+
+      <transition 
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+      >
+        <div class="alert alert-primary" v-if="mostrar">Animações no VueJS</div>
+      </transition>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      mostrar: true
+    }
+  },
+  methods: {
+    beforeEnter () {
+      console.log('beforeEnter')
+    },
+    enter () {
+      console.log('enter')
+    },
+    afterEnter () {
+      console.log('afterEnter')
+    },
+    enterCancelled () {
+      console.log('enterCancelled')
+    },
+    beforeLeave () {
+      console.log('beforeLeave')
+    },
+    leave () {
+      console.log('leave')
+    },
+    afterLeave () {
+      console.log('afterLeave')
+    },
+    leaveCancelled () {
+      console.log('leaveCancelled')
+    }
+  }
+}
+</script>
+
+<style>
+  body {
+    overflow: hidden;
+  }
+</style>
+
+<style scoped>
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 1s;
+  }
+
+  .zoom-enter, .zoom-leave-to {
+    transform: scale(0);
+  }
+
+  .zoom-enter-active, .zoom-leave-active {
+    transition: transform 0.5s;
+  }
+
+  .slide-enter, .slide-leave-to {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-transition 0.7s;
+    transition: opacity 0.7s;
+  }
+
+  .slide-leave-active {
+    animation: slide-transition 0.7s reverse;
+    transition: opacity 0.7s;
+  }
+
+  @keyframes slide-transition {
+    0% {
+      transform: translateX(-100px);
+    }
+
+    100% {
+      transform: translateX(0px);
+    }
+  }
+</style>
